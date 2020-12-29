6e39b70e7c5cb26bb836d3d2316d4e32c3bfd11d
sleepygloa <sleepygloa@gmail.com>
2020-12-29 10:47:01 +0900

    Initial Commit

---
+++ FirebaseScratch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseScratch
+//
+//  Created by seonho Kim on 2020/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
